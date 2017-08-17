40916eaa5cdbfb910075950f6209dd41385bbbb1
BespIV <snake07.07@mail.ru>
2017-08-17 20:01:00 +0400

    rewrite hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 def application(environ, start_response):
-    data = ''
-    for line in environ["QUERY_STRING"].split("&"):
-        data = data+line+"\n"
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return data
+    return body

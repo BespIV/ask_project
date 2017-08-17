a2a5bf7c643958a746304a4470afbfc53b0a4841
BespIV <snake07.07@mail.ru>
2017-08-17 17:32:39 +0400

    wsgi application on python

---
+++ hello.py
@@ -0,0 +1,16 @@
+
+def hello(environ, start_response):   
+
+    status = "200 OK"
+    
+    headers = [
+    ('Content-type', 'text/plain')
+    ]
+
+    if environ['QUERY_STRING'] != '':
+        msg = "\n".join(environ.get('QUERY_STRING').split("&"))
+    else:
+        msg = ""
+
+    start_response(status, headers)
+    return [ msg ]

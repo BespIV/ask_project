d73d648a0c79d45aab4ab04dbe1d89f5f594064f
BespIV <snake07.07@mail.ru>
2017-08-17 19:56:35 +0400

    rewrite hello

--- hello.py
+++ hello.py
@@ -1,18 +1,6 @@
-
-def app( environ, start_response ):   
-
-    status = "200 OK"
-
-    headers = [
-    ('Content-type', 'text/plain')
-    ]
-
-    if environ['QUERY_STRING'] != '':
-        for line in environ["QUERY_STRING"].split("&"):
-            msg = msg+line+"\n"
-        
-    else:
-        msg = " "
-
-    start_response(status, headers)
-    return msg
+def application(environ, start_response):
+    data = ''
+    for line in environ["QUERY_STRING"].split("&"):
+        data = data+line+"\n"
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return data

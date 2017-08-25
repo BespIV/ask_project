92ce0c90fafc2e0d02d949770be8d2f3f47d51cb
Иван <snake07.07@mail.ru>
2017-08-25 21:52:37 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,6 +2,5 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-cd /home/box/web/ask
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf ask.ask.wsgi:application &
 

fea2d5b5d734dd3887514fb15da76692583d3be2
BespIV <snake07.07@mail.ru>
2017-08-30 17:07:28 +0300

    add models.py in qa_app, changed settings of django and writed mysql shell for creation db

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+	def popular(self):
+		return self.order_by('-rating')
+
+class Question(models.Model):
+	objects = QuestionManager()
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add = True)
+	rating = models.IntegerField()
+	author = models.ForeignField(User)
+	likes = models.ForeignField(Likes)
+
+class Likes(models.Model):
+	question = models.ForeignField(Question, related_name = "like_question")
+	user = models.ForeignField(User)
+
+class Answer(modles.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add = True)
+	question = models.ForeignField(Question)
+	author = models.ForeignField(User)
